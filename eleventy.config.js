395db656f71504d03d905f80cc3ca562ade4f4ec
Ricky de Laveaga <git@rdela.com>
2024-08-05 00:18:04 -0700

    marketing team polish, tugboat component import
    added
    - base.webc layout
    - base.css styles
    - metadata.js
    
    add tugboat components
    - accessible-emoji
    - visually-hidden
    - footer.webc (customized)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,16 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import emojiShortName from "emoji-short-name";
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 export default function (eleventyConfig) {
 	eleventyConfig.ignores.add("./README.md");
-	eleventyConfig.addWatchTarget("./_components/**/*.css");
+	eleventyConfig.addWatchTarget("_components/**/*.css");
 	eleventyConfig.addPlugin(pluginWebc, {
-		components: ["./_components/**/*.webc"],
+		components: ["_components/**/*.webc"],
+	});
+
+	eleventyConfig.addJavaScriptFunction("emojiShortName", (emoji) => {
+		return emojiShortName[emoji];
 	});
 
 	return {
